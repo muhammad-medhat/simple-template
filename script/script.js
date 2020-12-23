c0f7e8d49b7eb4251e53865eb4cae999be152f4b
Muhammad Medhat <muhammad.medhat@gmail.com>
2020-12-23 06:06:54 +0200

    Adding skills functionality

--- script/script.js
+++ script/script.js
@@ -121,7 +121,52 @@ colorsList.forEach(li=>{
         })
     }
 })
+/************************************************************************************************
+ * skills functions
+ */
 
+/**
+ * first approach
+ * in this approach the event is fired when the whole skills div is visible in the viewport
+ */
+                // let target = document.querySelector('#skills')
+                // // options for the IntersectionObserver
+                // let options = {
+                //     rootMargin: '10px',
+                //     threshold: 0.5
+                // }
+
+                // let observer = new IntersectionObserver((entries) => {
+                //     entries.forEach(entry => {
+                //         console.log(entry.target)
+                //         const progSpan = entry.target.querySelector('.skill-progress span')
+                //         progSpan.style.width  = progSpan.dataset.progress
+                //     })
+                // }, options);
+                // target.querySelectorAll('.skill-box').forEach(el=>{
+                //     observer.observe(el)
+                // })
+/**
+ * second approach
+ * in this approach the event is fired when a single skill-box div is visible in the viewport
+ */
+const target = document.querySelectorAll('.skill-box')
+const observer = new IntersectionObserver(entries =>{
+    console.log(entries)
+    entries.forEach(entry=>{
+        const progSpan = entry.target.querySelector('.skill-progress span')
+        progSpan.style.width  = progSpan.dataset.progress
+    })
+})
+// console.log(target)
+target.forEach(t=>{
+    // console.log(t)
+    observer.observe(t)
+   
+})
+/*
+  * helper functions
+  */
 //style for selector
 function changeStyle(selector, style){
 
