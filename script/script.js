626a1a7c0725d85a6bae6458f328f90185cc5290
Muhammad Medhat <muhammad.medhat@gmail.com>
2020-12-24 01:00:46 +0200

    create a gallery

--- script/script.js
+++ script/script.js
@@ -152,7 +152,7 @@ colorsList.forEach(li=>{
  */
 const target = document.querySelectorAll('.skill-box')
 const observer = new IntersectionObserver(entries =>{
-    console.log(entries)
+    //  console.log(entries)
     entries.forEach(entry=>{
         const progSpan = entry.target.querySelector('.skill-progress span')
         progSpan.style.width  = progSpan.dataset.progress
@@ -164,7 +164,47 @@ target.forEach(t=>{
     observer.observe(t)
    
 })
-/*
+/**************************************************************************************************
+ * Gallery
+ */
+
+const gallery = document.querySelectorAll('.gallery .img-box img')
+gallery.forEach( img => {
+    img.addEventListener('click', e=>{
+        // console.log(e.target)
+        //create an overlay
+        const overlay = document.createElement('div')
+        overlay.className = 'popup'
+        document.body.appendChild(overlay)
+
+        //creating the popup-box
+        popBox = createPopupBox(img, overlay)
+
+
+        //creating close button
+        const cls = document.createElement('span')
+        
+        cls.innerText = 'X'
+        cls.className = 'btn-close'
+        popBox.appendChild(cls)
+        document.querySelector('.btn-close').addEventListener('click', e=>{
+            document.querySelector('.popup').remove()
+        })
+
+
+    })
+})
+function createPopupBox(img, overlay){
+    const popBox = document.createElement('div')
+    popBox.className = 'img-box'
+    const image = document.createElement('img')
+    image.src   = img.src
+    popBox.appendChild(img)
+    overlay.appendChild(popBox)
+    return popBox
+}
+
+/* ******************************************************************************* 
   * helper functions
   */
 //style for selector
